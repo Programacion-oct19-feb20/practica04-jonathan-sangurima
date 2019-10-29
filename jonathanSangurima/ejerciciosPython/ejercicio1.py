f7ee702d3c6b1090e3c98ff081f72e7c4eff3bf5
jonathan-sangurima <jonathanalexissangurimajara@gmail.com>
2019-10-29 07:41:54 -0500

    hola mundo

---
+++ jonathanSangurima/ejerciciosPython/ejercicio1.py
@@ -0,0 +1,15 @@
+"""
+    @autor: reroes
+    nombre: ejercicio1.py
+    descripción: ...
+"""
+
+nombre = "Jonathan Sangurima"
+edad = 19
+
+#print(nombre + "--" + edad)
+print(nombre)
+print(edad)
+
+
+
